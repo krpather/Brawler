c37a4f22c10f603675b198d92faa9fe9b0202dc1
Keanan Pather <keananpather@Keanans-MacBook-Air.local>
2022-11-18 00:27:05 +0200

    added game bg and basic movement

---
+++ fighter.py
@@ -0,0 +1,47 @@
+import pygame
+
+class Fighter():
+    def __init__(self,x,y):
+        self.rect = pygame.Rect((x,y, 80, 180))
+        self.vel_y = 0
+
+    def move(self,screen_width, screen_height):
+        SPEED = 10
+
+
+        GRAVITY = 2
+        dx = 0
+        dy = 0
+
+        # get keypresses
+        key = pygame.key.get_pressed()
+
+        #movement
+        if key[pygame.K_a]:
+            dx = -SPEED
+        if key[pygame.K_d]:
+            dx = SPEED
+        #jump
+        if key[pygame.K_w]:
+            self.vel_y = -30
+
+        #apply gravity
+        self.vel_y += GRAVITY
+        dy += self.vel_y
+
+        #keep player on screen
+        if self.rect.left + dx < 0:
+            dx = -self.rect.left
+        if self.rect.right + dx > screen_width:
+            dx = screen_width - self.rect.right
+        if self.rect.bottom + dy > screen_height - 110:
+            self.vel_y = 0
+            dy = screen_height - 110 - self.rect.bottom
+
+
+        # update player position
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def draw(self,surface):
+        pygame.draw.rect(surface, (255,0,0), self.rect)
---
+++ main.py
@@ -0,0 +1,52 @@
+import pygame
+from fighter import Fighter
+
+pygame.init()
+
+# create game window
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Brawler")
+
+#set framerate
+clock = pygame.time.Clock()
+FPS = 60
+
+# load bg image
+bg_image = pygame.image.load("assets/images/background/background.jpg").convert_alpha()
+
+# function for drawing background
+def draw_bg():
+    scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.blit(scaled_bg, (0,0))
+
+fighter_1 = Fighter(200, 310)
+fighter_2 = Fighter(700, 310)
+
+
+#game loop
+run = True
+while run:
+    clock.tick(FPS)
+    # draw bg
+    draw_bg()
+
+    # move fighters
+    fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+    # draw fighters
+    fighter_1.draw(screen)
+    fighter_2.draw(screen)
+
+    # event handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run =False
+
+    # update display
+    pygame.display.update()
+
+# exit pygame
+pygame.quit()
